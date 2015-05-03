c0e7e5a30b39ac1c929e810fbe002485e388f568
Brandon Kirklen <brandonkirklen@gmail.com>
2015-05-03 12:25:33 -0700

    Expanded HangmanWord holder class to contain information relevant to setting the difficulty.

--- HangmanWord.java
+++ HangmanWord.java
@@ -2,9 +2,11 @@
  * Created by Brandon Kirklen on 5/2/2015.
  * Declaration of class "HangmanWord" which holds a given word as well as a difficulty value.
  */
-public class HangmanWord<Word,Difficulty> {
+public class HangmanWord<Word,Difficulty,guessesNeeded,wrongGuesses> {
     private String Word;
     private int Difficulty;
+    private int guessesNeeded;
+    private String wrongGuesses;
 
     public HangmanWord(String Word, int Difficulty)
     {
@@ -19,6 +21,16 @@ public class HangmanWord<Word,Difficulty> {
     public void setDifficulty(int difficulty) {
         Difficulty = difficulty;
     }
+
+    public void setGuessesNeeded(int guessesNeeded)
+    {
+        this.guessesNeeded=guessesNeeded;
+    }
+
+    public void setWrongGuesses(String wrongGuesses)
+    {
+        this.wrongGuesses = wrongGuesses;
+    }
     public String getWord()
     {
         return Word;
@@ -26,5 +38,15 @@ public class HangmanWord<Word,Difficulty> {
 
     public int getDifficulty() {
         return Difficulty;
+    }
+
+    public int getGuessesNeeded()
+    {
+        return guessesNeeded;
+    }
+
+    public String getWrongGuesses()
+    {
+        return wrongGuesses;
     }
 }
