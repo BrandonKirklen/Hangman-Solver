bbb42dbf2c138329fe910df2ae4829d54ebcbe18
crobin10 <collierjrobinson@gmail.com>
2015-05-03 14:44:41 -0700

    Update HangmanFigure.java
    Updated figure, still working on it. Taking suggestions for any other changes we could make to the stick figure/gallows.
    I still need to edit the frame/add color if you guys think we need color or a different background

--- HangmanFigure.java
+++ HangmanFigure.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.RenderingHints;
 import javax.swing.JPanel;
 
 public class HangmanFigure extends JPanel {
@@ -16,56 +18,128 @@ public class HangmanFigure extends JPanel {
 	}
 	
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.BLACK);
+		//set graphics to graphics 2D
+		Graphics2D g2 = (Graphics2D) g;
 		
-		// base
+		//set line type to rounded line for cleaner look
+		BasicStroke rounded = new BasicStroke(3,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND);
+		g2.setStroke(rounded);
+
+		//turn on anti-aliasing to clean up circles
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				            RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		//set color to black
+		g2.setColor(Color.BLACK);
+		
+		// first guess
 		if(guesses > 0) {
-			g.drawLine(1, 299, 299, 299);
+			//draws base
+			g2.drawLine(1, 299, 299, 299);
+	
+			//draws gallows
+			g2.drawRect(10, 10, 15, 299);
+			g2.drawRect(10, 10, 160, 15);
+
+			//draws rope
+			g.drawLine(150, 25, 150, 70);
 		}
 		
-		// right wall
+		// second guess
 		if(guesses > 1) {
-			g.drawLine(299, 299, 299, 1);
+			//draws head
+			g2.drawOval(150-25, 70, 50, 50);
+			
+			//eyes
+			g2.drawOval(140,87,2,4);
+			g2.drawOval(160,87,2,4);
+			
+			//mouth - happy
+		    g2.drawArc(140, 100, 21, 12, 180, 180);	
 		}
 		
-		// top line
+		// third guess
 		if(guesses > 2) {
-			g.drawLine(150, 1, 299, 1);
+			//body
+			g2.drawLine(150, 120, 150, 200);
 		}
 		
-		// hanging line
+		// fourth guess
 		if(guesses > 3) {
-			g.drawLine(150, 1, 150, 70);
+			//left arm
+			g2.drawLine(150, 140, 140, 185);
 		}
 		
-		// face
+		// fifth guess
 		if(guesses > 4) {
-			g.drawOval(150-25, 70, 50, 50);
+			//right arm
+			g2.drawLine(150, 140, 160, 185);
 		}
 		
-		// body
+		// sixth guess
 		if(guesses > 5) {
-			g.drawLine(150, 120, 150, 200);
+			//change face to worried ._.
+			g2.clearRect(138, 98, 26, 16);
+			g2.drawLine(140,105,160,105);	
 		}
 		
-		// left hand
+		// seventh guess
 		if(guesses > 6) {
-			g.drawLine(150, 150, 110, 140);
+			//left leg
+			g2.drawLine(150, 200, 135, 250);
 		}
 		
-		// right hand
+		//eighth guess
 		if(guesses > 7) {
-			g.drawLine(150, 150, 190, 140);
+			//right leg
+			g2.drawLine(150, 200, 165, 250);
 		}
 		
-		// left leg
+		//ninth guess
 		if(guesses > 8) {
-			g.drawLine(150, 200, 120, 250);
+			g2.clearRect(138, 98, 26, 16);
+			g2.drawArc(140, 105, 21, 8, -180, -180);		
 		}
 		
-		// right leg
+		//tenth guess
 		if(guesses > 9) {
-			g.drawLine(150, 200, 180, 250);
+			//resets panel
+			g2.clearRect(0,0,300,300);
+			
+			//re-draws base
+			g2.drawLine(1, 299, 299, 299);
+	
+			//re-draws gallows
+			g2.drawRect(10, 10, 15, 299);
+			g2.drawRect(10, 10, 160, 15);
+
+			//draws longer rope
+			g.drawLine(150, 25, 150, 85);
+			
+			//re-draws head
+			g2.drawOval(150-25, 85, 50, 50);
+			
+			//draws dead face
+			g2.drawLine(140,99,145,107);				//left eye
+			g2.drawLine(140,107,145,99);				//left eye
+			g2.drawLine(155,99,160,107);				//right eye
+			g2.drawLine(155,107,160,99);				//right eye
+			g2.drawArc(140, 120, 21, 8, -180, -180);	        //mouth
+			
+			//re-draws body
+			g2.drawLine(150, 135, 150, 215);
+			
+			//re-draws left arm
+			g2.drawLine(150, 155, 140, 200);
+			
+			//re-draws right arm
+			g2.drawLine(150, 155, 160, 200);
+			
+			//re-draws left leg
+			g2.drawLine(150, 215, 135, 265);
+			
+			//re-draws right leg
+			g2.drawLine(150, 215, 165, 265);
 		}
 	}
 	
