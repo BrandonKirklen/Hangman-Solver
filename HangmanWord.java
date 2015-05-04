d796d7a89d94192ddda59e62152182475f0548af
Brandon Kirklen <brandonkirklen@gmail.com>
2015-05-03 19:15:27 -0700

    Changed difficulty constructor

--- HangmanWord.java
+++ HangmanWord.java
@@ -2,24 +2,18 @@
  * Created by Brandon Kirklen on 5/2/2015.
  * Declaration of class "HangmanWord" which holds a given word as well as a difficulty value.
  */
-public class HangmanWord<Word,Difficulty,guessesNeeded,wrongGuesses> {
+public class HangmanWord {
     private String Word;
-    private int Difficulty;
     private int guessesNeeded;
     private String wrongGuesses;
 
-    public HangmanWord(String Word, int Difficulty)
+    public HangmanWord(String Word)
     {
         this.Word = Word;
-        this.Difficulty = Difficulty;
     }
 
     public void setWord(String word) {
         Word = word;
-    }
-
-    public void setDifficulty(int difficulty) {
-        Difficulty = difficulty;
     }
 
     public void setGuessesNeeded(int guessesNeeded)
@@ -34,10 +28,6 @@ public class HangmanWord<Word,Difficulty,guessesNeeded,wrongGuesses> {
     public String getWord()
     {
         return Word;
-    }
-
-    public int getDifficulty() {
-        return Difficulty;
     }
 
     public int getGuessesNeeded()
